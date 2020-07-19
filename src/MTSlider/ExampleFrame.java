75fddbdf190d558f03e1d5806b28779fb0aa2bdc
yaso al_zokari <yaso2com@gmail.com>
2020-07-19 18:56:30 +0200

    Add System exit to kill the Java TM platform process

--- src/MTSlider/ExampleFrame.java
+++ src/MTSlider/ExampleFrame.java
@@ -10,6 +10,8 @@ import multiThumbSlider.lib.SliderPanel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 /**
  *
@@ -23,6 +25,15 @@ public class ExampleFrame
     public ExampleFrame()
     {
         initGUI();
+        addWindowListener(new WindowAdapter()
+        {
+            @Override
+            public void windowClosing(WindowEvent e)
+            {
+                super.windowClosing(e);
+                System.exit(0);
+            }
+        });
     }
 
     private void initGUI()
